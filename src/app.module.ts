18d6f5b1663abbf6f86072a75ee8edcaf6665eee
oskim <bkks1004@naver.com>
2023-11-06 23:14:38 +0900

    Set: temp Backend setting
    - yarn, db, jest setting

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV == 'dev' ? '.dev.env' : '.prod.env',
+      isGlobal: true,
+      // Joi 추가 시 사용
+      // validationSchema: Joi.object({
+      //   NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+      //   DB_HOST: Joi.string().required(),
+      //   DB_PORT: Joi.string().required(),
+      //   DB_USERNAME: Joi.string().required(),
+      //   DB_PASSWD: Joi.string().required(),
+      //   DB_DATABASE: Joi.string().required(),
+      // }),
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWD,
+      database: process.env.DB_DATABASE,
+      models: [],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
