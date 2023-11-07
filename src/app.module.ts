65d7b9665647ac56686cd4f79e58d336870c12cd
oskim <bkks1004@naver.com>
2023-11-07 22:14:23 +0900

    Set: temp  ValidationPipe, etc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { SequelizeModule } from '@nestjs/sequelize'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 import { User } from './user/user.entity'
 import { UserModule } from './user/user.module'
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors(); // cors 활성화
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
+  const port: number = configService.get('BACK_PORT')
+
+  app.enableCors({
+    origin: true, //url을 넣어도 됨.
+    credentials: true,
+  }) // cors 활성화
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // auto-transformation(request의 payload가 nest의 dto에 맞게 type를 변환 시켜주는 옵션)
+      whitelist: true, // payload에서 dto에 없는 속성을 제거시켜주는 옵션
+      transformOptions: { enableImplicitConversion: true }, // string에서 number, boolean, array 등으로 암시적으로 변환시켜주는 기능
+      forbidNonWhitelisted: true, // dto에 없는 속성을 제거시켜주는 대신 error를 throw해주는 옵션
+    })
+  )
+  await app.listen(port)
 }
-bootstrap();
+bootstrap()
