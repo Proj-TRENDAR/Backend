08e49689f45a4bf0bd6db88cbd86c8bc91454251
oskim <bkks1004@naver.com>
2023-11-28 22:45:08 +0900

    Feat: user swagger 작성
    임시

--- dto/create-user.dto.ts
+++ dto/create-user.dto.ts
@@ -1,11 +1,42 @@
-import { IsNotEmpty } from 'class-validator'
+import { IsString, IsEmail, IsOptional, IsNotEmpty, Length } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'tester',
+    description: '유저 아이디',
+    required: true,
+  })
+  @IsString()
   @IsNotEmpty()
+  @Length(45)
   id: string
+
+  @ApiProperty({
+    example: '테스터',
+    description: '유저 이름',
+    required: true,
+  })
+  @IsString()
   @IsNotEmpty()
+  @Length(45)
   name: string
+
+  @ApiProperty({
+    example: 'tester@gamil.com',
+    description: '유저 이메일',
+    required: true,
+  })
+  @IsEmail()
   @IsNotEmpty()
+  @Length(100)
   email: string
+
+  @ApiProperty({
+    example: 'image url...',
+    description: '유저 이미지',
+  })
+  @IsString()
+  @IsOptional()
   imgUrl: string
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,29 +2,50 @@ import { Body, Controller, Delete, Get, Param, Post, UsePipes, ValidationPipe } 
 import { CreateUserDto } from '../../dto/create-user.dto'
 import { User } from './user.model'
 import { UserService } from './user.service'
+import { ApiTags, ApiOperation, ApiCreatedResponse } from '@nestjs/swagger'
 
 @Controller('user')
+@ApiTags('User API')
 export class UserController {
   // 의존성 주입 (controller에서 만든 service를 사용할 수 있게 해주는 작업)
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @ApiOperation({ summary: '전체 유저 조회', description: '전체 유저 조회 API' })
   findAll(): Promise<User[]> {
     return this.userService.findAll()
   }
 
   @Get(':id')
+  @ApiOperation({ summary: '특정 유저 조회', description: '특정 유저 조회 API' })
   findOne(@Param('id') id: string): Promise<User> {
     return this.userService.findOne(id)
   }
 
   @Post()
+  @ApiOperation({ summary: '유저 생성', description: '유저 생성 API' })
+  @ApiCreatedResponse({
+    description: '성공 시(example)',
+    schema: {
+      example: {
+        id: 'tester',
+        name: '테스터',
+        email: 'tester@gamil.com',
+        img_url: null,
+        updated_at: '2023-11-28T11:50:58.172Z',
+        created_at: '2023-11-28T11:50:58.172Z',
+        refresh_token: null,
+        thema_color: 1,
+      },
+    },
+  })
   @UsePipes(ValidationPipe)
   create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.userService.create(createUserDto)
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: '유저 삭제', description: '유저 삭제 API' })
   remove(@Param('id') id: string): Promise<void> {
     return this.userService.remove(id)
   }
