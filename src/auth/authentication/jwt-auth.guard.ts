ee33b1305de34d1591c373c7954a3e7c3408c808
ohseong_kim <bkks1004@naver.com>
2024-01-22 20:51:32 +0900

    Feat: jwt guard canActivate, handleRequest 추가

--- src/auth/authentication/jwt-auth.guard.ts
+++ src/auth/authentication/jwt-auth.guard.ts
@@ -1,5 +1,17 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    // Custom authentication logic
+    return super.canActivate(context)
+  }
+  handleRequest(err, user, info) {
+    // Handle any authentication errors
+    if (err || !user) {
+      throw err || new UnauthorizedException()
+    }
+    return user
+  }
+}
