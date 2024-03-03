f7bacb5da5fb4077e3517a32cefc3de9a5975074
oskim <bkks1004@naver.com>
2024-03-03 21:42:34 +0900

    Set: production db config

--- src/database/config.js
+++ src/database/config.js
@@ -20,10 +20,10 @@ module.exports = {
   production: {
     // temp
     dialect: 'mysql',
-    host: process.env.DB_HOST,
-    port: +process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWD,
-    database: process.env.DB_DATABASE,
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'test123',
+    database: 'trendar',
   },
 }
