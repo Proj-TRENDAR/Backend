ceb449ca760789e202674b41c052a74a86b73de3
oskim <bkks1004@naver.com>
2024-05-26 15:48:33 +0900

    fix: 모듈 분리 및 순환참조 오류 수정

--- src/routine/routine-day/routine-day.module.ts
+++ src/routine/routine-day/routine-day.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { RoutineDayController } from './routine-day.controller'
 import { RoutineDayService } from './routine-day.service'
-import { RoutineModule } from '../routine.module'
+import { RoutineDay } from 'models'
 
 @Module({
-  imports: [SequelizeModule.forFeature([RoutineModule])],
+  imports: [SequelizeModule.forFeature([RoutineDay])],
   controllers: [RoutineDayController],
   providers: [RoutineDayService],
+  exports: [RoutineDayService],
 })
 export class RoutineDayModule {}
--- src/routine/routine.module.ts
+++ src/routine/routine.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { RoutineController } from 'src/routine/routine.controller'
 import { RoutineService } from 'src/routine/routine.service'
-import { RoutineDayService } from 'src/routine/routine-day/routine-day.service'
-import { RoutineCompletedService } from 'src/routine/routine-completed/routine-completed.service'
-import { Routine, RoutineCompleted, RoutineDay } from 'models'
+import { Routine } from 'models'
 import { RoutineCompletedModule } from './routine-completed/routine-completed.module'
+import { RoutineDayModule } from './routine-day/routine-day.module'
 
 @Module({
-  imports: [SequelizeModule.forFeature([Routine, RoutineDay]), RoutineCompletedModule],
+  imports: [SequelizeModule.forFeature([Routine]), RoutineCompletedModule, RoutineDayModule],
   controllers: [RoutineController],
-  providers: [RoutineService, RoutineDayService],
+  providers: [RoutineService],
 })
 export class RoutineModule {}
