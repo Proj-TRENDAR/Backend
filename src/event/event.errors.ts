21a6e90a904e61a3ea47a99c8028f1563bdead8c
oskim <bkks1004@naver.com>
2024-10-09 16:44:20 +0900

    fix: typo

--- src/event/event.errors.ts
+++ src/event/event.errors.ts
@@ -4,7 +4,7 @@ export const EVENT_ERRORS = {
   NOT_FOUND: 'EVENT_NOT_FOUND',
 }
 
-const DEFAULT_EVENT_NOT_FOUND_MESSAGE = '존재하지 않는 루틴입니다.'
+const DEFAULT_EVENT_NOT_FOUND_MESSAGE = '존재하지 않는 일정입니다.'
 
 export class EventNotFoundException extends NotFoundException {
   constructor(message: string = DEFAULT_EVENT_NOT_FOUND_MESSAGE) {
